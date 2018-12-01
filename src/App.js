e133acb856d177422ffa1e47159392f1887e7499
byronluk <byronluk@gmail.com>
2018-12-01 15:01:51 -0800

    setting up landing page and routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
 import Navbar from "./components/Navbar";
 import Home from "./views/Home";
+import Signup from "./views/Signup";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
-        <Router>
-            <Route path="/" component={Home} />
-        </Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/signup" component={Signup} />
       </div>
     );
   }
